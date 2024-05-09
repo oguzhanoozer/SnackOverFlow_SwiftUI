bdae73e6ba46eb86207a593be1c80d556cdebc58
Oguzhan Ozer <oguzoozer@gmail.com>
2024-05-09 11:52:01 +0300

    Initial Commit

---
+++ snackoverflow/snackoverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  snackoverflowApp.swift
+//  snackoverflow
+//
+//  Created by oguzhan on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct snackoverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
